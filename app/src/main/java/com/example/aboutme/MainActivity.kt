627b2249c89ecd4d806b28ac663002b9d5db133f
mohamed ayman <>
2024-09-05 14:46:30 +0300

    first

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.aboutme
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var dataBinding: ViewDataBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dataBinding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
